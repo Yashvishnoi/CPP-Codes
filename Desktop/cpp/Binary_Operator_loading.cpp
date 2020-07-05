aaab98c12e2b369574dce3a0ea1e856a73b335a9
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-05 17:53:32 +0530

    Added the + operator overloading

--- Desktop/cpp/Binary_Operator_loading.cpp
+++ Desktop/cpp/Binary_Operator_loading.cpp
@@ -12,13 +12,23 @@ class loc {
 			cout<<"The value of a is : "<<a<<endl;
 			cout<<"The value of b is : "<<b<<endl;
 		}
+		loc operator+(loc ob2);
 };
 
+loc loc::operator+(loc ob2 ){
+	loc temp;
+	
+	temp.a=ob2.a+a;
+	temp.b=ob2.b+b;
+	return temp;
+}
 int main() {
-	loc ob1, ob2;
+	loc ob1, ob2,ob3;
 	ob1.set(10,20);
 	ob1.get();
 	ob2.set(2,4);
 	ob2.get();
+	ob3=ob1+ob2;
+	ob3.get();
 	return 0;
 }
