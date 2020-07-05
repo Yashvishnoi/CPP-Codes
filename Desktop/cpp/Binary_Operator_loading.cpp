582dd2c1f9495282126ac9400f7ae57a3a10aace
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-05 17:41:40 +0530

    Added the class and its member function

--- Desktop/cpp/Binary_Operator_loading.cpp
+++ Desktop/cpp/Binary_Operator_loading.cpp
@@ -3,9 +3,22 @@ using namespace std;
 
 class loc {
 	int a,b;
+	public:
+		void set(int x, int y){
+			a=x;
+			b=y;
+		}
+		void get( ) {
+			cout<<"The value of a is : "<<a<<endl;
+			cout<<"The value of b is : "<<b<<endl;
+		}
 };
 
 int main() {
-	
+	loc ob1, ob2;
+	ob1.set(10,20);
+	ob1.get();
+	ob2.set(2,4);
+	ob2.get();
 	return 0;
 }
