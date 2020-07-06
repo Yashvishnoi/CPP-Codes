1dab5d6cf3511f72dd87421ad48465d005223bba
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-06 21:29:04 +0530

    Created the file of name some_file.txt

--- Desktop/cpp/How_to_open_or_create_file.cpp
+++ Desktop/cpp/How_to_open_or_create_file.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 int main() {
+	fstream some_file;
+	some_file.open("some_file.txt" , ios::out);
+	
+	// "ios::out" to write in file
+	// use open() function
+	
+	if(!some_file) {
+		cout<<"FAIL ! "<<"\n";
+	}
+	else {
+		cout<<"File created" <<"\n";
+		some_file.close();
+	// file creation completed
+	}
 	
 	return 0;
 }
