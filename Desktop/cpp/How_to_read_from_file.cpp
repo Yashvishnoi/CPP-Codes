8089572fc59859f54fded3d523b2aafea95828c5
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-06 21:42:49 +0530

    Code how to read from a file

--- Desktop/cpp/How_to_read_from_file.cpp
+++ Desktop/cpp/How_to_read_from_file.cpp
@@ -7,5 +7,17 @@ int main() {
 	some_file.open("some_file.txt", ios::in);
 	// in is used to read from a file
 	
+	if(!some_file) {
+		cout<<"Fail"<<"\n";
+	}
+	
+	else {
+		char read_file;
+		while (!some_file.eof()) {
+			some_file>>read_file;
+			cout<<read_file;
+			
+		}
+	}
 	return 0;
 }
