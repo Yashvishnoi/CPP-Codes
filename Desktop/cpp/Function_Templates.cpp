f1016f327d11bdab1179ec4436246715c930d54f
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-07 22:40:30 +0530

    Added the line 17-20

--- Desktop/cpp/Function_Templates.cpp
+++ Desktop/cpp/Function_Templates.cpp
@@ -14,9 +14,13 @@ int main() {
 	int z1 = sum<int>(x1 , y1) ; 
 	
 	 // Here we call the function "sum" (type int)
+	double x2 = 5.8;
+	double y2 = 5.1;
+	double z2 = sum<double>(x2 , y2) ;
+	 // here we call the function "sum" (type double)
 	 
 	cout<<z1 <<"\n";
-	
+	cout<<z2<<"\n";
 	return 0;
 	
 }
