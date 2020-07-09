2038fd9b1c9451b96cb0c0e0772d86d1bc499971
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-09 23:50:29 +0530

    Added the main function and use template as int

--- Desktop/cpp/Class_Templates.cpp
+++ Desktop/cpp/Class_Templates.cpp
@@ -15,3 +15,15 @@ template <class T> Sum<T>::Sum(T x, T y) {
 	this->x = x;
 	this -> y =y;
 }
+
+template <class T> T Sum<T>::getSum() {
+	T z;
+	z=x+y;
+	return z;
+}
+
+int main() {
+	Sum <int> z1(5,5);
+	cout<<z1.getSum() <<"\n";
+	return 0;
+}
