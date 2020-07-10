83ad363471eea0753cf8da824d3edae1773bd40d
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-10 22:48:17 +0530

    Added the float function

--- Desktop/cpp/Function_Templates.cpp
+++ Desktop/cpp/Function_Templates.cpp
@@ -18,9 +18,12 @@ int main() {
 	double y2 = 5.1;
 	double z2 = sum<double>(x2 , y2) ;
 	 // here we call the function "sum" (type double)
-	 
+	float x3 = 5.8f;
+	float y3 = 5.1f;
+	float z3 = sum<float>(x2 , y2) ;	 
 	cout<<z1 <<"\n";
 	cout<<z2<<"\n";
+	cout<<z3<<"\n";
 	return 0;
 	
 }
