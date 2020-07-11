c77d6e93855e6694191352d52d9f4bf31383311d
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-11 19:21:41 +0530

    Added the base class

--- Desktop/cpp/Virtual_function.cpp
+++ Desktop/cpp/Virtual_function.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 
+class Base {
+	int varBase;
+	public:
+		void display() {
+			cout<<"Base class variable varBase "<<varBase<<"\n";
+			}
+};
+
 int main() {
 	
 	return 0;
