791276090f01f514ba62bbb62855f2f2659808b7
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-11 19:57:53 +0530

    Made the pointer to derived class

---
+++ Desktop/cpp/Pointer_to_derived_class.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Base {
+	public:
+		int varBase = 1;
+		void display() {
+			cout<<"Base class variable varBase "<<varBase<<"\n";
+	}
+};
+
+class Derived : public Base {
+	public:
+		int varDerived = 2; 
+		void display() {
+			cout<<"Base class variable varBase "<<varBase<<"\n";
+			cout<<"Derived class variable varDerived "<<varDerived <<"\n";
+		}
+};
+
+int main() {
+	Base *b_pointer ;
+	Derived d_object;
+	
+	b_pointer=&d_object;
+	b_pointer->display();
+	return 0;
+}
