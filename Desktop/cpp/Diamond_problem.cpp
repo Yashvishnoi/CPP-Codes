200ea37672b3d9b6c4bf1b9e8310b1f80aece3b9
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-11 22:09:11 +0530

    Code on to print diamond

--- Desktop/cpp/Diamond_problem.cpp
+++ Desktop/cpp/Diamond_problem.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
  int main() {
- 	
+ 	int rows, i, j , space=1;
+ 	cout<<"Rows"<<"\n";
+ 	cin>>rows;
+ 	space= rows-1;
+ 	for(i=1;i<=rows;i++){
+ 		for(j=1;j<=space;j++) {
+ 			cout<<" ";
+		 }
+		 space--;
+		 for(j=1;j<=(2*i-1);j++) {
+		 	cout<<"* ";
+		 }
+		 cout<<"\n";
+	 }
+	 space = 1;
+	 for(i=1;i<(rows-1);i++) {
+	 	for(j=1;j<=space;j++) {
+	 		cout<<" ";
+		 }
+		 space ++;
+		 for(j=1;j<=(2*(rows-1)-1);j++) {
+		 cout<<"* ";
+		 }
+		 cout<<"\n";		 
+	 }
  	return 0;
  }
